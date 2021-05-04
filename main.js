23195ce59feaa93e9eecfb33986d4454053a1f59
wejdev <wejdev@gmail.com>
2021-05-03 22:06:27 -0700

    Implement move cards and update players in game

--- main.js
+++ main.js
@@ -180,93 +180,97 @@ function displayPlayerBench() {
 // This function is called each time a player button is clicked. A player
 // button being clicked indicates the players is either moving to the court
 // or to the bench for a water break
-function movePlayer() {
-    // Do not let the coach change players during a quarter
+// This function is called each time a player button is selected. A player's
+// button being selected indicates that the player either moving to the
+// court or moving to the bench for a water break.
+function movePlayers() {
+    // Don't let the coach change players during a quarter.
+    if(quarterInPlay) {
+        return;
+    }
 
-
-
-
-    // Get the div in which this button currently is (either bench or court).
-
+    // Get the div where this button currently is (either bench or court).
+    var parentDiv = this.parentElement;
 
     // Check whether the player is currently on the bench.
-
+    if(parentDiv.id == 'playersOnBench') {
         // If there are already five players on the court, don't let the player
-        // move to the court; alert the coach that there are enough players.
-
-
-
-
+        // move to the court, and alert the coach that there are enough players.
+        if(playersOnCourt >= maxPlayersOnCourt){
+            alert('You can only have ' + maxPlayersOnCourt + ' players on the court at a time.');
+        } else {
             // If there is room on the court, update the number of players on
             // the court, and update the average PER for the quarter based on
             // this player moving to the court.
+            playersOnCourt++;
+            quarterPER += playerMap.get(this.id)[currentQuarter];
+            quarterAvePER = quarterPER / playersOnCourt;
+            document.getElementById('currentPER').innerText = 'Current PER: '+ quarterAvePER.toPrecision(4);
 
-
-
-
-
-
-            // Move the player to the court
-
-
-
+            // Move the player to the court.
+            document.getElementById('playersOnCourt').appendChild(this);
+        }
+    } else {
         // If the player is being taken off the court for a water break, decrement
         // the number of players on the bench and remove the player's PER from the
         // average.
+        playersOnCourt--;
 
-
-
-
-
-
+        if(playersOnCourt != 0) {
+            quarterPER -= playerMap.get(this.id)[currentQuarter];
+            quarterAvePER = quarterPER / playersOnCourt;
+        } else {
             // If there are no more players on the court, set the values to 0.
-
-
-
+            quarterPER = 0;
+            quarterAvePER = 0;
+        }
 
         // Update the PER average. This might result in a zero value if your team is particularly tired.
-
+        document.getElementById('currentPER').innerText = 'Current PER: '+ quarterAvePER.toPrecision(4);
 
         // Move the player to the bench.
-
-
+        document.getElementById('playersOnBench').appendChild(this);
+    }
 }
 
+// At the start of each quarter, do two things:
+// 1. Ensure the players currently on the court have the correct PER represented
+// 2. Update the stats for each player for the current quarter.
 // At the start of each quarter, do two things:
 // 1. Ensure the players currently on the court have the correct PER represented
 // 2. Update the stats for each player for the current quarter.
 function updateCardsInGame() {
     // For each player, update their player stat card to show PER for that player for
     // a specific quarter.
-
-
-
+    for (let [playerName, playerStats] of playerMap.entries()) {
+        document.getElementById(playerName + '_card').children[1].innerText = 'PER: '+playerStats[currentQuarter].toPrecision(4);
+    }
 
     // Reset the current quarter's total PER.
-
-
+    quarterPER = 0;
+    quarterAvePER = 0;
 
     // Get a list of all the players currently on the court.
-
+    var currentPlayers = document.getElementById('playersOnCourt').children;
 
     // Loop through each of the players currently on the court.
-
+    for(var playerIndex = 0; playerIndex < currentPlayers.length; playerIndex++) {
         // Get the name of the player
-
+        var playerName = currentPlayers[playerIndex].id;
 
         // Get the PER for the player
-
+        var playerPER = playerMap.get(playerName)[currentQuarter];
 
         // Add the PER to the quarter PER total
-
-
+        quarterPER += playerPER;
+    }
 
     // Get the average PER for the start of the quarter.
-
+    quarterAvePER = quarterPER / playersOnCourt;
 
     // Update Current PER with the new average PER for the quarter now that the
     // stats have been updated.
-
+    document.getElementById('currentPER').innerText = 'Current PER: '+ quarterAvePER.toPrecision(4);
 }
 
 // At the end of each quarter, show the coach the average PER
